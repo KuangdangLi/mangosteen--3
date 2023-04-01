392f762d034bfaa28186c9a548e7923c910b7a68
riverjoy <tosakarinmylove@gmail.com>
2023-04-01 15:21:03 +0800

    设置build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/mangosteen--3/dist/',
   plugins: [vue()],
   server: {
     port: 9999 // 更改为所需的端口号
