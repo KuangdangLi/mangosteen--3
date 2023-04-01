a7d581a29eca8c863b9232d23188dffde1192735
riverjoy <tosakarinmylove@gmail.com>
2023-04-01 15:53:34 +0800

    引入tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'/mangosteen--3/dist/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueJsx({
+      transformOn:true,
+      mergeProps:true
+    })
+  ],
   server: {
     port: 9999 // 更改为所需的端口号
   }
