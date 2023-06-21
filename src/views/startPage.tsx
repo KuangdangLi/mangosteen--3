1ee4ee208254813f08cc4f6693e94d670c56b900
riverjoy <tosakarinmylove@gmail.com>
2023-06-21 11:50:04 +0800

    添加floatButton组件

--- src/views/startPage.tsx
+++ src/views/startPage.tsx
@@ -1,6 +1,7 @@
 import { defineComponent } from 'vue';
 import { Button } from '../shared/Button';
 import s from './startPage.module.scss';
+import { FloatButton } from '../shared/FloatButton';
 export const startPage = defineComponent({
  setup: (props, context) => {
     const onClick = ()=>{
@@ -8,9 +9,13 @@ export const startPage = defineComponent({
         
     }
   return () => (
+    <div>
     <div class={s.button_wrapper}>
         <Button class={s.button} onClick={onClick}>test</Button>
    </div>
+    <FloatButton iconName="add"></FloatButton>
+    </div>
+
   )
  }
 })
