86cba97065c8f7502ea0652b3d4491a7a81947f6
riverjoy <tosakarinmylove@gmail.com>
2023-07-15 21:54:00 +0800

    完成TagEdit页面

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,4 +1,9 @@
 import { defineComponent, PropType } from 'vue';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Icon } from '../../shared/Icon';
+import { TagForm } from './TagForm';
+import s from './Tag.module.scss' 
+import { Button } from '../../shared/Button';
 export const TagEdit = defineComponent({
  props: {
   name: {
@@ -7,7 +12,20 @@ export const TagEdit = defineComponent({
  },
  setup: (props, context) => {
   return () => (
-   <div></div>
+   <MainLayout>
+        {{
+            title: ()=> '修改标签',
+            icon:()=> <Icon name='menu' onClick={() => { }}></Icon>,
+            default: ()=>(<>
+                <TagForm />
+                <div class={s.actions}>
+                    <Button level='danger' class={s.removeTags} onClick={() => { }}>删除标签</Button>
+                    <Button level='danger' class={s.removeTagsAndItems} onClick={() => { }}>删除标签和记账</Button>
+                </div>
+            </>
+            )
+        }}
+   </MainLayout>
   )
  }
 })
--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -37,6 +37,6 @@ export const routes = [
     ]},
     {path:'/tags',component:TagPage,children:[
         {path:'create',component:TagCreate},
-        {path:':id',component:TagEdit}
+        {path:':id/edit',component:TagEdit}
     ]}
 ]
