2f5e7dde37c729e519bbe306a7fbf69ebc44d405
riverjoy <tosakarinmylove@gmail.com>
2023-08-27 20:53:30 +0800

    删除标签

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -4,7 +4,9 @@ import { TagForm } from './TagForm';
 import s from './Tag.module.scss' 
 import { Button } from '../../shared/Button';
 import { BackIcon } from '../../shared/BackIcon';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
+import { Dialog } from 'vant';
+import { http } from '../../shared/Http';
 export const TagEdit = defineComponent({
  props: {
   name: {
@@ -13,9 +15,23 @@ export const TagEdit = defineComponent({
  },
  setup: (props, context) => {
     const route = useRoute()
+    const router = useRouter()
     const numberId:number = parseInt(route.params.id!.toString())
     if(Number.isNaN(numberId)){
         return ()=> <div>id不存在</div>
+    }
+    const onError = ()=>{
+        Dialog.alert({ title:'提示',message:'删除失败' })
+    }
+    const onDelete =async (options?: {withItems?:boolean})=>{
+        await Dialog.confirm({
+            title:'确认',
+            message:'你真的要删除吗'
+        })
+        await http.delete(`/tags/${numberId}`,{
+            withItems: options?.withItems ? 'true' : 'false'
+        }).catch(onError)
+        router.back()
     }
   return () => (
    <MainLayout>
@@ -25,8 +41,10 @@ export const TagEdit = defineComponent({
             default: ()=>(<>
                 <TagForm id={numberId} />
                 <div class={s.actions}>
-                    <Button level='danger' class={s.removeTags} onClick={() => { }}>删除标签</Button>
-                    <Button level='danger' class={s.removeTagsAndItems} onClick={() => { }}>删除标签和记账</Button>
+                    <Button level='danger' class={s.removeTags} 
+                    onClick={() => onDelete()}>删除标签</Button>
+                    <Button level='danger' class={s.removeTagsAndItems} 
+                    onClick={() => onDelete({withItems:true})}>删除标签和记账</Button>
                 </div>
             </>
             )
