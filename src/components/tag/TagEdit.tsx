159704fbac3f983e0db09bda4336906d16d78191
riverjoy <tosakarinmylove@gmail.com>
2023-09-27 22:04:00 +0800

    删除标签（对应记账也会被删除）

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,58 +1,3 @@
-// import { defineComponent, PropType } from 'vue';
-// import { MainLayout } from '../../layouts/MainLayout';
-// import { TagForm } from './TagForm';
-// import s from './Tag.module.scss' 
-// import { Button } from '../../shared/Button';
-// import { BackIcon } from '../../shared/BackIcon';
-// import { useRoute, useRouter } from 'vue-router';
-// import { Dialog } from 'vant';
-// import { http } from '../../shared/Http';
-// export const TagEdit = defineComponent({
-//  props: {
-//   name: {
-//    type: String as PropType<string>
-//   }
-//  },
-//  setup: (props, context) => {
-//     const route = useRoute()
-//     const router = useRouter()
-//     const numberId:number = parseInt(route.params.id!.toString())
-//     if(Number.isNaN(numberId)){
-//         return ()=> <div>id不存在</div>
-//     }
-//     const onError = ()=>{
-//         Dialog.alert({ title:'提示',message:'删除失败' })
-//     }
-//     const onDelete =async (options?: {withItems?:boolean})=>{
-//         await Dialog.confirm({
-//             title:'确认',
-//             message:'你真的要删除吗'
-//         })
-//         await http.delete(`/tags/${numberId}`,{
-//             with_items: options?.withItems ? 'true' : 'false'
-//         },{_autoLoading: true}).catch(onError)
-//         router.back()
-//     }
-//   return () => (
-//    <MainLayout>
-//         {{
-//             title: ()=> '编辑标签',
-//             icon:()=> <BackIcon />,
-//             default: ()=>(<>
-//                 <TagForm id={numberId} />
-//                 <div class={s.actions}>
-//                     <Button level='danger' class={s.removeTags} 
-//                     onClick={() => onDelete()}>删除标签</Button>
-//                     <Button level='danger' class={s.removeTagsAndItems} 
-//                     onClick={() => onDelete({withItems:true})}>删除标签和记账</Button>
-//                 </div>
-//             </>
-//             )
-//         }}
-//    </MainLayout>
-//   )
-//  }
-// })
 import { Dialog } from 'vant'
 import { defineComponent } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
@@ -96,17 +41,10 @@ export const TagEdit = defineComponent({
               <div class={s.actions}>
                 <Button
                   level="danger"
-                  class={s.removeTags}
-                  onClick={() => onDelete()}
-                >
-                  删除标签
-                </Button>
-                <Button
-                  level="danger"
                   class={s.removeTagsAndItems}
                   onClick={() => onDelete({ withItems: true })}
                 >
-                  删除标签和记账
+                  删除标签（对应记账也会被删除）
                 </Button>
               </div>
             </>
