00ca4f2528231518e1cdcd319303af959f3115ec
riverjoy <tosakarinmylove@gmail.com>
2023-09-30 22:48:27 +0800

    mock 不会在生产环境中打包

--- vite.config.ts
+++ vite.config.ts
@@ -5,40 +5,50 @@ import styleImport, { VantResolve } from "vite-plugin-style-import";
 import { svgstore } from "./src/vite_plugins/svgstore";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id: any) {
-          if (id.includes("echarts")) {
-            return "echarts";
+export default defineConfig(({ command }) => {
+  return {
+    define:
+      command === "build"
+        ? {
+            DEBUG: false,
           }
-          if (id.includes("mock") || id.includes("faker")) {
-            return "mock";
-          }
-          if (id.includes("vant")) {
-            return "vant";
-          }
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
+        : {
+            DEBUG: true,
+          },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id: any) {
+            if (id.includes("echarts")) {
+              return "echarts";
+            }
+            if (id.includes("mock") || id.includes("faker")) {
+              return "mock";
+            }
+            if (id.includes("vant")) {
+              return "vant";
+            }
+            if (id.includes("node_modules")) {
+              return "vendor";
+            }
+          },
         },
       },
     },
-  },
-  base: "/dist/",
-  plugins: [
-    vue(),
-    vueJsx({
-      transformOn: true,
-      mergeProps: true,
-    }),
-    svgstore(),
-    styleImport({
-      resolves: [VantResolve()],
-    }), //引入vant样式
-  ],
-  server: {
-    port: 9999, // 更改为所需的端口号
-  },
+    base: "/dist/",
+    plugins: [
+      vue(),
+      vueJsx({
+        transformOn: true,
+        mergeProps: true,
+      }),
+      svgstore(),
+      styleImport({
+        resolves: [VantResolve()],
+      }), //引入vant样式
+    ],
+    server: {
+      port: 9999, // 更改为所需的端口号
+    },
+  };
 });
